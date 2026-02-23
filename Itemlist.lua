03a83c2b1a8172533afac05c1c54f10e7161f0a3
Obisedor <obisedor1@outlook.com>
2026-02-23 02:21:26 +0100

    Update Itemlist.lua

--- Itemlist.lua
+++ Itemlist.lua
@@ -1,5 +1,45 @@
 local Itemlist = {
-  e = "e"
-}
 
-return Itemlist
+
+
+
+    StandardPlotEquipment = {
+        ["FoodBowl"] = {
+            PetInteraction = {
+                EatDefault = {Hunger = 100},
+                EatFreshVegetables = {Hunger = 175, Health = 1}
+            }
+        }
+    },
+
+
+
+    PlotUpgrades = {
+        ["Pet toy"] = {
+            Description = "",
+            ShopInfo = {
+                Price = 67,
+                Currency = "Money"
+            },
+            PetInteraction = {
+                Default = {Happiness = 35, Energy = -5}
+            }
+        },
+
+        ["Playground"] = {
+            Description = "",
+            ShopInfo = {
+                Price = 676,
+                Currency = "Money"
+            },
+            PetInteraction = {
+                PlaySlide = {Happiness = 15, Energy = -5},
+                PlaySwing = {Happiness = 20, Energy = -15}
+            }
+        }
+    }
+
+
+
+
+}
